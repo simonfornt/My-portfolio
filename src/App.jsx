89ce26f77bcbc90ccfb86304b4eb2b17c094eef8
simonfotnt <simonpiar98@gamil.com>
2025-03-11 10:38:03 +0600

    correct spellling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar';
-import Footer from './components/Footer';
+import Footer from './Components/Footer';
 import Home from './pages/Home';
 import About from './pages/About';
 import Blog from './pages/Blog';
